ac0853bc4bdb0750be98d68659b4582ad96af1cd
SAURABH ARYA <42316833+souravarya07@users.noreply.github.com>
2025-05-28 14:35:14 +0530

    Initial project setup

---
+++ StudyPal/src/main/java/com/study/assistant/StudyPal/StudyPalApplication.java
@@ -0,0 +1,13 @@
+package com.study.assistant.StudyPal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPalApplication.class, args);
+	}
+
+}
